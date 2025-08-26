3d7b9d02f7f1f7030ae0304b9c5887ddeea07dbe
Franco D'Agostino <144281050+Francodago@users.noreply.github.com>
2025-08-26 12:26:59 -0400

    added basic funcitonality for todos module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import loadPage from './loadPage';
 import blankProjectLoad from './project-manager';
-import todos from './todos';
+import { createTodos } from './todos';
 import projects from './projects';
 
 //*call dom manipulation module to take care of the UI
 loadPage();
-//*call blank project on first land
+//*call blank project on first land and start with the default project
 blankProjectLoad();
 
 //*include modules for manipulating  the projects and todos
 
 projects();
-todos();
+const todos = [
+  createTodos(
+    'Buy milk',
+    '2% protein',
+    '2025-09-05',
+    'medium',
+    true,
+    'I want protein',
+    ['go to freshco', 'buy yogurt']
+  ),
+  createTodos(
+    'Buy rice',
+    'Basmati',
+    '2025-010-25',
+    'low',
+    undefined,
+    'I need the carbs so i need rice',
+    ['go to freshco', 'buy rice']
+  ),
+];
+console.log(todos);
+const todo1 = createTodos(
+  'Buy milk',
+  '2% protein',
+  '2025-09-05',
+  'medium',
+  true,
+  'I want protein',
+  ['go to freshco', 'buy yogurt']
+);
+console.log(todo1);
+
+todo1.toggleComplete();
--- src/todos.js
+++ src/todos.js
@@ -1,2 +1,30 @@
-const todos = () => {};
-export default todos;
+export function createTodos( //*factory function create todos
+  title,
+  description = '',
+  dueDate,
+  priority,
+  isComplete = false,
+  notes = '',
+  checklist = []
+) {
+  //todo create an object with the methdos i will share between instances
+  const todo = Object.create(todoMethods);
+  todo.title = title;
+  todo.description = description;
+  todo.dueDate = dueDate;
+  todo.priority = priority;
+  todo.isComplete = isComplete;
+  todo.notes = notes;
+  todo.checklist = checklist;
+  return todo;
+}
+const todoMethods = {
+  //*todos prototype object that is going to have all the shareable methdos for each createTodos instance
+  toggleComplete() {
+    this.isComplete = !this.isComplete;
+    return this.isComplete;
+  },
+  addChecklistItem(item) {
+    this.checklist.push(item);
+  },
+};
